b51dfa16824478fe2c9c8dbc1998015fb088ef0a
Sam <asamadans@gmail.com>
2024-12-09 23:03:56 +0530

    chore: forge init

---
+++ escrow.sol
@@ -0,0 +1,27 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity >=0.8.0 <0.9.0;
+
+event Deposited(address indexed _from, uint _value);
+event claimed(address indexed _to, uint _value);
+
+contract Escrow {
+    address payable public sender;
+    address payable public recipient;
+
+    struct Vault {
+        uint amount;
+        address sender;
+        address recipient;
+    }    
+
+    function deposit(uint amount) public payable{
+        
+
+        emit Deposited(msg.sender, msg.value);
+    }
+
+    function withdraw() public payable {
+        
+        emit claimed(msg.sender, msg.value);
+    }
+}
