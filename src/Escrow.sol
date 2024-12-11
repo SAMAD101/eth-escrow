6339f5e2db9f438e8bf26c024cd9d712bdc209a5
Sam <asamadans@gmail.com>
2024-12-11 12:29:38 +0530

    openzepplin-contracts

--- src/Escrow.sol
+++ src/Escrow.sol
@@ -1,6 +1,8 @@
 // SPDX-License-Identifier: GPL-3.0
 pragma solidity >=0.8.13;
 
+import {ERC20} from "lib/openzeppelin-contracts/contracts/token/ERC721/ERC721.sol";
+
 event Deposited(address indexed _from, uint _value);
 event claimed(address indexed _to, uint _value);
 
