adb663078d6f7ed4283b8c488ae1be684762a7f2
Sam <asamadans@gmail.com>
2024-12-13 18:51:06 +0530

    minor refactors

--- test/Escrow.t.sol
+++ test/Escrow.t.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
 
-import {Test, console} from "forge-std/Test.sol";
+import {Test, console} from "lib/forge-std/src/Test.sol";
 import {Escrow} from "../src/Escrow.sol";
 
 contract EscrowTest is Test {
