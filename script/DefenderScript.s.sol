0c368f3c50996e91cf92301672bf87c1c90758e5
Sam <asamadans@gmail.com>
2024-12-14 23:43:50 +0530

    Chnages in scripts/                                                                                                                       [23:42:51] dquote> DefenderScript moved to scripts/ dquote> Deploy script not needed

--- script/DefenderScript.s.sol
+++ script/DefenderScript.s.sol
@@ -7,7 +7,8 @@ import {console} from "lib/forge-std/src/console.sol";
 import {Defender, ApprovalProcessResponse} from "lib/openzeppelin-foundry-upgrades/src/Defender.sol";
 import {Upgrades, Options} from "lib/openzeppelin-foundry-upgrades/src/Upgrades.sol";
 
-import {EscrowContract} from "./Escrow.sol";
+import {EscrowContract} from "src/Escrow.sol";
+import {ERC20EscrowContract} from "src/ERC20Escrow.sol";
 
 contract DefenderScript is Script {
     function setUp() public {}
@@ -22,12 +23,22 @@ contract DefenderScript is Script {
         Options memory opts;
         opts.defender.useDefenderDeploy = false;
 
-        address proxy = Upgrades.deployUUPSProxy(
+        // Deploy ETH Escrow
+        address ethEscrowProxy = Upgrades.deployUUPSProxy(
             "Escrow.sol:EscrowContract",
             abi.encodeCall(EscrowContract.initialize, (payable(msg.sender))),
             opts
         );
 
-        console.log("Deployed proxy to address", proxy);
+        console.log("Deployed ETH Escrow proxy to address", ethEscrowProxy);
+
+        // Deploy ERC20 Escrow
+        address erc20EscrowProxy = Upgrades.deployUUPSProxy(
+            "ERC20Escrow.sol:ERC20EscrowContract",
+            abi.encodeCall(ERC20EscrowContract.initialize, (payable(msg.sender))),
+            opts
+        );
+
+        console.log("Deployed ERC20 Escrow proxy to address", erc20EscrowProxy);
     }
 }
