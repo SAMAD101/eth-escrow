9fd4f5d0e8eae7505c50a00d2281fbf3e736eb3b
Sam <asamadans@gmail.com>
2024-12-16 13:11:52 +0530

    Almost complete

--- README.md
+++ README.md
@@ -4,14 +4,37 @@ A gas efficient solidity smart contract for an escrow on ethereum. Supports ERC2
 
 ## Features:
 - Allows the sender to designate a set amount of ETH (or an ERC20 token).
-- The recipient user can claim or refund back to the sender the designated amount of ETH (or an ERC20 token).
+- The recipient user can claim or refund back to the sender the designated amount of ETH (or an ERC20 token). Automated refunds using Chainlink.
 - Unclaimed funds automatically gets refunded back to the sender.
-- Upgradable contract...
-- Role-based access control...
+- Upgradable contract, uses UUPS.
+- Reentrancy protection.
 
 ## Implementation:
 - Uses [Foundary framework](https://book.getfoundry.sh/) for development and testing
-- Uses [OpenZeppelin](https://www.openzeppelin.com/)...
+- Uses [OpenZeppelin](https://www.openzeppelin.com/) for safe and secure smart contracts and upgradability
 - Comprehensive test suite
-- Secure, maintainable, and modular Solidity code, with great organization of code
+- Secure, maintainable, and modular Solidity code, with great organization
 - Follows best practices and style guide for Solidity
+
+## Setup
+
+1. Clone the repository
+
+```bash
+git clone https://github.com/SAMAD101/eth-escrow.git
+```
+
+2. Environment variables
+
+- SEPOLIA_RPC_URL
+- PRIVATE_KEY
+
+3. Build and deploy
+
+```bash
+forge script script/DefenderScript.s.sol --force --rpc-url $SEPOLIA_RPC_URL
+```
+
+## Tests
+
+to be added...
--- script/DefenderScript.s.sol
+++ script/DefenderScript.s.sol
@@ -4,7 +4,6 @@ pragma solidity ^0.8.20;
 import {Script} from "lib/forge-std/src/Script.sol";
 import {console} from "lib/forge-std/src/console.sol";
 
-import {Defender, ApprovalProcessResponse} from "lib/openzeppelin-foundry-upgrades/src/Defender.sol";
 import {Upgrades, Options} from "lib/openzeppelin-foundry-upgrades/src/Upgrades.sol";
 
 import {EscrowContract} from "src/Escrow.sol";
@@ -14,20 +13,13 @@ contract DefenderScript is Script {
     function setUp() public {}
 
     function run() public {
-        ApprovalProcessResponse memory upgradeApprovalProcess = Defender.getUpgradeApprovalProcess();
-
-        if (upgradeApprovalProcess.via == address(0)) {
-            revert(string.concat("Upgrade approval process with id ", upgradeApprovalProcess.approvalProcessId, " has no assigned address"));
-        }
-
-        Options memory opts;
-        opts.defender.useDefenderDeploy = false;
+        uint256 deployerPrivateKey = vm.envUint("PRIVATE_KEY");
+        vm.startBroadcast(deployerPrivateKey);
 
         // Deploy ETH Escrow
         address ethEscrowProxy = Upgrades.deployUUPSProxy(
             "Escrow.sol:EscrowContract",
-            abi.encodeCall(EscrowContract.initialize, (payable(msg.sender))),
-            opts
+            abi.encodeCall(EscrowContract.initialize, (payable(msg.sender)))
         );
 
         console.log("Deployed ETH Escrow proxy to address", ethEscrowProxy);
@@ -35,8 +27,7 @@ contract DefenderScript is Script {
         // Deploy ERC20 Escrow
         address erc20EscrowProxy = Upgrades.deployUUPSProxy(
             "ERC20Escrow.sol:ERC20EscrowContract",
-            abi.encodeCall(ERC20EscrowContract.initialize, (payable(msg.sender))),
-            opts
+            abi.encodeCall(ERC20EscrowContract.initialize, (payable(msg.sender)))
         );
 
         console.log("Deployed ERC20 Escrow proxy to address", erc20EscrowProxy);
